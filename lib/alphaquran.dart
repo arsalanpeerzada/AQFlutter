01027ef54e634fb66816576d5a7706b272742533
arsalanpeerzada <peerzada_1994@live.com>
2024-05-14 00:57:32 +0500

    14-5-2024

--- lib/alphaquran.dart
+++ lib/alphaquran.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 
 class AlphaQuran extends StatefulWidget {
@@ -13,21 +15,27 @@ class _AlphaQuranState extends State<AlphaQuran> {
   Color white = Color(0xFFFFFFFF);
 
 
+  // Sample data for the list
+  Map<String, String> chapters = {
+    "1": "The Opening",
+    "2": "The Cow",
+    "3": "The Family of Imran",
+    // Add more chapters or verses as needed
+  };
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         width: double.infinity,
         height: double.infinity,
-        color: background,
+        color: white,
         child: Column(
           children: <Widget>[
-            Padding(padding:
-            EdgeInsets.only(top: MediaQuery.of(context).padding.top)),
             Container(
               width: double.infinity,
               height: 250,
-              margin: EdgeInsets.only(bottom: 30),
               decoration: BoxDecoration(
                 image: DecorationImage(
                   image: AssetImage("images/header.png"),
@@ -40,7 +48,7 @@ class _AlphaQuranState extends State<AlphaQuran> {
                 children: [
                   Text(
                     "Alphabetical Quran",
-                    textAlign: TextAlign.center,
+                    textAlign: TextAlign.left,
                     style: TextStyle(
                         fontSize: 30,
                         color: white),
@@ -48,8 +56,39 @@ class _AlphaQuranState extends State<AlphaQuran> {
                 ],
               ),
             ),
+            Container(
+              child: Expanded(// Use Expanded to fill the remaining space
+                child: ListView(
+                  children: chapters.entries.map((entry) {
+                    return _buildCustomListItem({entry.key: entry.value});
+                  }).toList(),
+                ),
+              ),
+            ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildCustomListItem(Map<String, String> chapter) {
+    // Assuming the map has only one entry: {'chapterName': 'description'}
+    String chapterName = chapter.keys.first;
+    String description = chapter.values.first;
+
+    return Container(
+      color: white,
+      padding: EdgeInsets.all(8), // Added padding for better UI
+      child: Row(
+        children: [
+          SizedBox(width: 10),
+          Text(chapterName, style: TextStyle(color: Colors.black, fontSize: 18)),
+          SizedBox(width: 10),
+          Expanded(
+            child: Text(description, style: TextStyle(color: Colors.grey, fontSize: 14)), // Optional: display description
+          ),
+          Icon(Icons.book, color: fontGold, size: 30),
+        ],
       ),
     );
   }
